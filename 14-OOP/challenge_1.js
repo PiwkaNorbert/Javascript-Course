1d9f3a382fe594f7949748fe5b3588c35d1eb112
Norbert Piwka <piwkanorbert@gmail.com>
2023-01-31 13:13:46 +0100

    Chapter 14 OOP

---
+++ 14-OOP/challenge_1.js
@@ -0,0 +1,49 @@
+'use strict';
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+GOOD LUCK 😀
+
+
+*/
+
+const Car = function(make, speed){
+  this.make = make
+  this.speed = speed
+
+}
+
+const BMW = new Car('BMW', 120);
+const Mercedes = new Car('Mercedes', 95);
+
+Car.prototype.accelrate = function(){
+this.speed +=10
+  console.log(`The ${this.make} is accelerated, and is now going ${this.speed}km/h`);
+
+}
+Car.prototype.break = function(){
+this.speed-=5
+  console.log(`The ${this.make} is broke, and is now going ${this.speed}km/h`);
+
+}
+
+Mercedes.accelrate()
+Mercedes.break()
+Mercedes.accelrate()
+Mercedes.accelrate()
+Mercedes.accelrate()
+
+BMW.accelrate()
+BMW.break()
+BMW.break()
+BMW.break()
+BMW.break()
+BMW.break()
---
+++ 14-OOP/script.js
@@ -0,0 +1,152 @@
+'use strict';
+/*
+
+const Person = function(firstName,birthYear){
+// Instance properties
+    this.firstName = firstName
+    this.birthYear = birthYear
+
+    // Never do this!!!! call method in class
+    // this.calcAge = function(){
+    //   console.log(2037 - this.birthYear);
+    // }
+}
+
+
+const norbert = new Person('Norbert',1998)
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const mateusz = new Person('Mateusz', 1997)
+const piotrek = new Person('Piotrek', 1991)
+
+console.log(norbert,mateusz,piotrek);
+
+const jay = 'jay'
+console.log(norbert instanceof Person);
+console.log(jay instanceof Person);
+
+// Prototypes
+
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function(){
+
+    console.log(2023 - this.birthYear);
+  }
+
+norbert.calcAge()
+mateusz.calcAge()
+piotrek.calcAge()
+
+console.log(norbert.__proto__);
+console.log(norbert.__proto__ ===Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(norbert )); 
+
+Person.prototype.species = 'Homo Sapiens'
+console.log(norbert.species, mateusz.species);
+
+console.log(norbert.hasOwnProperty('firstName'));
+console.log(norbert.hasOwnProperty('species'));
+
+console.log(norbert.__proto__);
+// Object.prototype (to p of prototype chain)
+console.log(norbert.__proto__.__proto__);
+console.log(norbert.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const array = [2,3,4,676,7,233,2,34,] // new Array === []
+console.log( array);
+console.log( array.__proto__ === Array.prototype);
+
+console.log( array.__proto__.__proto__);
+
+Array.prototype.unique = function(){
+
+  return [...new Set(this)]
+}
+console.log(array.unique());
+
+const h1 = document.querySelector('h1')
+
+console.dir(x=>x+1)
+*/
+
+////////////////////////////////
+
+// ES6 Classes
+
+////////////////////////////////
+
+
+// Class expression
+// const PersonCl = class {}
+// Class declaration
+class PersonCl {
+  constructor(fullName, birthYear){
+    this.fullName = fullName
+    this.birthYear = birthYear
+  }
+
+  // methods will be added to .prototype property
+  calcAge(){
+    console.log(2037 -this.birthYear); 
+  }
+  greet(){
+    console.log(`Hey ${this.fullName}`);
+    
+    }
+    get age(){
+      return 2037 - this.birthYear
+    }
+    set fullName(name){
+      console.log(name);
+      if(name.includes(' '))this._fullName = name
+      else alert(`${name} is not a full name!`)
+    }
+    get fullName(){
+      return this._fullName
+    }
+}
+
+// PersonCl.prototype.greet = function(){
+// console.log(`Hey ${this.firstName}`);
+
+// }
+const jessica = new PersonCl('Jessica Davis',1996)
+const walter = new PersonCl('Walter',1994)
+console.log(jessica);
+console.log(walter);
+
+jessica.calcAge()
+console.log(jessica.age );
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+jessica.greet()
+
+// 1. Classes are NOT hoisted
+// 2. Classes are first-class citizens
+// 3. Classes are executed in strict mode
+
+
+
+const account = {
+  owner : 'Norbert',
+  movements : [200,5003,120,230],
+
+  get lastest(){
+    return this.movements.slice(-1).pop()
+  },
+  set lastest(mov){
+    this.movements.push(mov)
+
+  }
+}
+
+console.log(account.lastest);
+console.log(account.lastest = 50);
+console.log(account.movements);

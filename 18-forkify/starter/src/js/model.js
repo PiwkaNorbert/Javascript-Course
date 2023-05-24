363bea031406fdbda81a3b79055e12a4cad82719
Norbert Piwka <piwkanorbert@gmail.com>
2023-05-24 10:05:25 +0200

    refactoring

---
+++ 18-forkify/starter/src/js/model.js
@@ -0,0 +1,28 @@
+import { API_URL } from './config';
+import { getJSON } from './helpers';
+
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const data = await getJSON(`${API_URL}/${id}`);
+
+    let { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(state.recipe);
+  } catch (error) {
+    // temp error handling
+    console.error(`${error} kambob`);
+  }
+};
